62f0e5e619638c2468b103a48f82ce3f73551569
Logistica5 <Logistica5@brkmegadan.local>
2020-03-29 18:49:14 -0300

    Backend, Frontend e Mobile da aplicação

---
+++ backend/src/routes.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const { celebrate, Segments, Joi } = require("celebrate");
+
+const OngController = require("./controller/OngController");
+const IncidentController = require("./controller/IncidentController");
+const ProfileController = require("./controller/ProfileController");
+const SessionController = require("./controller/SessionController");
+
+const routes = express.Router();
+
+routes.post("/sessions", SessionController.create);
+
+routes.get("/ongs", OngController.index);
+routes.post(
+  "/ongs",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string()
+        .required()
+        .email(),
+      whatsapp: Joi.string()
+        .required()
+        .min(10)
+        .max(11),
+      city: Joi.string().required(),
+      uf: Joi.string()
+        .required()
+        .length(2)
+    })
+  }),
+  OngController.create
+);
+
+routes.get(
+  "/profile",
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required()
+    }).unknown()
+  }),
+  ProfileController.index
+);
+
+routes.get(
+  "/incidents",
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number()
+    })
+  }),
+  IncidentController.index
+);
+routes.post("/incidents", IncidentController.create);
+routes.delete(
+  "/incidents/:id",
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required()
+    })
+  }),
+  IncidentController.delete
+);
+
+module.exports = routes;
